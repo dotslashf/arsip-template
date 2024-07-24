08186a406525044ce6063442275d8ca17c7eb0b3
fadhlu <38921923+dotslashf@users.noreply.github.com>
2024-07-24 21:59:31 +0800

    style: content to top

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,15 @@
-import Navbar from "./Navbar"
+import Navbar from "./Navbar";
 
 interface LayoutProps {
-    children: JSX.Element | JSX.Element[] | string
+  children: JSX.Element | JSX.Element[] | string;
 }
 export default function Layout(props: LayoutProps) {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-center text-primary">
-            <Navbar />
-            <div className="container flex flex-col items-center justify-center gap-6 px-4 py-16">
-                {props.children}
-            </div>
-        </main>
-    )
+  return (
+    <main className="flex min-h-screen flex-col items-center text-primary">
+      <Navbar />
+      <div className="container flex flex-col items-center justify-center gap-6 px-4 py-16">
+        {props.children}
+      </div>
+    </main>
+  );
 }
