30eb2262bd45c02b98fe1a838b119d5005b2bbc6
fadhlu <38921923+dotslashf@users.noreply.github.com>
2024-08-01 21:55:36 +0800

    style: remove unused

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Brand() {
   return (
     <Link href={"/"}>
-      <h1 className="dark: flex items-center justify-center text-2xl font-bold dark:text-foreground">
+      <h1 className="flex items-center justify-center text-2xl font-bold dark:text-foreground">
         <Package className="mr-3" />
         arsip
         <br />
