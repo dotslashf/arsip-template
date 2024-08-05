67c537075ed911b2d76d7f36eb1fefa0c8eee7be
fadhlu <38921923+dotslashf@users.noreply.github.com>
2024-08-05 21:03:36 +0800

    style: update color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,7 +21,7 @@ interface NavbarProps {
 }
 export default function Navbar({ session }: NavbarProps) {
   return (
-    <nav className="fixed inset-x-0 top-0 z-50 bg-white py-1 shadow dark:bg-gray-950">
+    <nav className="fixed inset-x-0 top-0 z-50 bg-white py-1 shadow dark:bg-card">
       <div className="container px-4 md:px-6">
         <div className="flex h-12 items-center">
           <Link
