595d4fc1a37ab8008e53d7e35b92b1f7910e0041
fadhlu <38921923+dotslashf@users.noreply.github.com>
2024-08-11 12:38:45 +0700

    chore: improve meta

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,8 +1,13 @@
+import { type Metadata } from "next";
 import Link from "next/link";
 import Brand from "~/components/Brand";
 import Layout from "~/components/Layout";
 import { HydrateClient } from "~/trpc/server";
 
+export const metadata: Metadata = {
+  title: "Kebijakan Privasi",
+  description: "Kebijakan Privasi arsip-template",
+};
 export default function PrivacyPolicyPage() {
   return (
     <HydrateClient>
