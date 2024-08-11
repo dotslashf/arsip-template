6a8ec6f8be96d3813e993e894fad2db2f74ec54d
fadhlu <38921923+dotslashf@users.noreply.github.com>
2024-08-11 13:32:16 +0700

    feat: add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,36 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://www.arsiptemplate.app/",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://www.arsiptemplate.app/tos",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.arsiptemplate.app/privacy-policy",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.arsiptemplate.app/ranking",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.arsiptemplate.app/changelog",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
+  ];
+}
