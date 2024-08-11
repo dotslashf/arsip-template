1368459b9fb6c17dccd03f9ef68448a799363908
fadhlu <38921923+dotslashf@users.noreply.github.com>
2024-08-11 12:27:13 +0700

    feat: improve card

--- src/components/CopyPastaCard.tsx
+++ src/components/CopyPastaCard.tsx
@@ -37,6 +37,7 @@ export interface CopyPastaCardWithTagsProps extends CopyPasta {
   };
   fullMode?: boolean;
   isApprovalMode?: boolean;
+  isCreatorAndDateShown?: boolean;
 }
 
 export interface CopyPastaProps {
@@ -46,6 +47,8 @@ export interface CopyPastaProps {
 export default function CopyPastaCard({ copyPastaProps }: CopyPastaProps) {
   const toast = useToast();
   const router = useRouter();
+  copyPastaProps.isCreatorAndDateShown =
+    copyPastaProps.isCreatorAndDateShown ?? true;
 
   function handleCopy() {
     navigator.clipboard
@@ -142,7 +145,7 @@ export default function CopyPastaCard({ copyPastaProps }: CopyPastaProps) {
             </div>
           )}
           <div className="mt-2 flex flex-col gap-4 text-sm text-secondary-foreground dark:text-muted-foreground lg:mt-4">
-            {copyPastaProps.fullMode && (
+            {copyPastaProps.isCreatorAndDateShown && (
               <div className="flex justify-between">
                 <Badge
                   variant={"outline"}
--- src/components/ProfileCopyPastaCard.tsx
+++ src/components/ProfileCopyPastaCard.tsx
@@ -62,7 +62,6 @@ export default function ProfileCopyPastaCard({
                       type === "disapproved" &&
                       !copy.fullMode &&
                       isApprovalMode,
-                    fullMode: type === "disapproved" && isApprovalMode,
                   }}
                 />
               );
