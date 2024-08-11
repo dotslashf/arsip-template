e0348359b70046d964e523456822178f71f23c19
fadhlu <38921923+dotslashf@users.noreply.github.com>
2024-08-11 09:30:07 +0700

    style: adjust for mobile position

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FeedbackFish } from "@feedback-fish/react";
-import { Dot } from "lucide-react";
+import { Dot, MessageSquareText } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import { type Session } from "next-auth";
@@ -13,7 +13,7 @@ export default function Footer({ session }: FooterProps) {
   return (
     <footer className="w-full bg-white py-6 shadow dark:bg-card">
       <div className="container flex items-center justify-center gap-4 px-4 md:px-6">
-        <div className="flex w-full items-center gap-4">
+        <div className="flex w-full flex-col items-center gap-4 lg:flex-row">
           <Link
             href="/tos"
             className="text-sm hover:underline"
@@ -29,10 +29,20 @@ export default function Footer({ session }: FooterProps) {
           >
             Kebijakan Privasi
           </Link>
+          <Dot className="hidden w-4 lg:block" />
+          <Link
+            href="/changelog"
+            className="text-sm hover:underline"
+            prefetch={false}
+          >
+            Changelog
+          </Link>
           {session && (
-            <div className="ml-auto">
+            <div className="lg:ml-auto">
               <FeedbackFish projectId="ee2e6f2b856911" userId={session.user.id}>
-                <Button>Send feedback</Button>
+                <Button>
+                  Ada Masukan? <MessageSquareText className="ml-2 w-4" />
+                </Button>
               </FeedbackFish>
             </div>
           )}
