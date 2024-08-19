c2c872bd9aef0ef034e80a4f26a61ad7c3977c0c
dotslashf <38921923+dotslashf@users.noreply.github.com>
2024-08-19 22:54:07 +0700

    feat: share cookie across sub domain

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -44,6 +44,20 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
+  cookies: {
+    sessionToken: {
+      name:
+        process.env.NODE_ENV === "production"
+          ? `__Secure-next-auth.session-token`
+          : `next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        path: "/",
+        secure: process.env.NODE_ENV === "production" ? true : false,
+      },
+    },
+  },
   callbacks: {
     session: async ({ session, user }) => {
       const account = await db.account.findFirst({
