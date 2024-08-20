820d60f7906d3d524a82e4110adf4ec9d8959fff
dotslashf <38921923+dotslashf@users.noreply.github.com>
2024-08-21 00:37:34 +0700

    feat: improve indexing

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/dashboard/",
+      disallow: ["/dashboard/", "/api/trpc/"],
     },
     sitemap: "https://arsiptemplate.app/sitemap.xml",
   };
