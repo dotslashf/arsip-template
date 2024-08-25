e2b68b6da3902969782216c2f977f086924b33bd
dotslashf <38921923+dotslashf@users.noreply.github.com>
2024-08-25 10:01:32 +0700

    chore: enable swf minify and remove unused package

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: true,
   output: "standalone",
   httpAgentOptions: {
     keepAlive: false,
