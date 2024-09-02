4e20cf7f4cbf937155c00b5d8dd201119975ac7c
dotslashf <38921923+dotslashf@users.noreply.github.com>
2024-09-02 15:37:35 +0700

    feat: improve og

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,18 +1,17 @@
 import { ImageResponse } from "next/og";
-// App router includes @vercel/og.
-// No need to install it.
+import { baseUrl } from "~/lib/constant";
 
 export const runtime = "edge";
 
-const getGeist = async () => {
-  const response = await fetch(new URL("./Geist-Regular.ttf", import.meta.url));
+export const getGeist = async () => {
+  const response = await fetch(`${baseUrl}/Geist-Regular.ttf`);
   const geist = await response.arrayBuffer();
 
   return geist;
 };
 
-const getGeistBold = async () => {
-  const response = await fetch(new URL("./Geist-Bold.ttf", import.meta.url));
+export const getGeistBold = async () => {
+  const response = await fetch(`${baseUrl}/Geist-Bold.ttf`);
   const geist = await response.arrayBuffer();
 
   return geist;
@@ -24,46 +23,44 @@ const ogCopyPastaId = (copyPasta?: string) => (
       height: "100%",
       width: "100%",
       display: "flex",
-      flexDirection: "column",
+      flexDirection: "row",
       alignItems: "center",
       justifyContent: "center",
       backgroundColor: "white",
       fontFamily: "Geist",
     }}
   >
-    <div tw="flex flex-col w-full h-full items-center justify-center bg-slate-50">
-      <div tw="flex w-full">
-        <div tw="flex flex-col flex-row w-full py-12 px-4 items-center justify-around p-8">
-          <h2 tw="pl-16 flex items-center justify-center text-5xl font-bold tracking-tight text-slate-800  max-w-xs w-full">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="mr-2"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              style={{
-                width: "2.5rem",
-                height: "2.5rem",
-                marginRight: "0.5rem",
-              }}
-            >
-              <path d="m7.5 4.27 9 5.15"></path>
-              <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z"></path>
-              <path d="m3.3 7 8.7 5 8.7-5"></path>
-              <path d="M12 22V12"></path>
-            </svg>
-            <span>arsip template</span>
-          </h2>
-          <div tw="h-full w-0.5 self-center bg-slate-600 dark:bg-white/10"></div>
-          <div tw="flex max-w-xs w-full text-slate-800 text-3xl text-center">
-            {copyPasta}
-          </div>
+    <div tw="flex w-full h-full items-center justify-center bg-slate-50">
+      <div tw="flex flex-col w-full items-center justify-around px-26">
+        <h2 tw="flex items-center justify-center text-6xl font-bold tracking-tight text-slate-800 max-w-xs w-full mb-16">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="mr-2"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            style={{
+              width: "4.5rem",
+              height: "4.5rem",
+              marginRight: "1rem",
+            }}
+          >
+            <path d="m7.5 4.27 9 5.15"></path>
+            <path d="M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z"></path>
+            <path d="m3.3 7 8.7 5 8.7-5"></path>
+            <path d="M12 22V12"></path>
+          </svg>
+          <span>arsip template</span>
+        </h2>
+        <div tw="flex w-full text-slate-800 text-3xl text-center mb-16">
+          {copyPasta}
         </div>
+        <div tw="h-1 w-1/4 self-center bg-slate-600 dark:bg-white/10"></div>
       </div>
     </div>
   </div>
@@ -83,7 +80,7 @@ const ogCopyPasta = () => (
     }}
   >
     <div tw="flex w-full flex-col -mt-8">
-      <div tw="flex w-full items-center justify-center p-8">
+      <div tw="flex w-full flex-col items-center justify-center p-8">
         <h2 tw="flex items-center justify-center text-7xl font-bold tracking-tight text-slate-800 max-w-xs">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -108,10 +105,10 @@ const ogCopyPasta = () => (
           </svg>
           <span>arsip template</span>
         </h2>
+        <span tw="w-full flex items-center justify-center text-4xl max-w-xl mt-8 text-center text-slate-800">
+          platform template(copy-pasta) netizen Indonesia 🇮🇩
+        </span>
       </div>
-      <span tw="flex items-center justify-center text-xl text-slate-800">
-        platform template(copy-pasta) netizen Indonesia 🇮🇩
-      </span>
     </div>
   </div>
 );
