21f9bcde82c9cb5b186ad3f104d024615bba6779
dotslashf <38921923+dotslashf@users.noreply.github.com>
2024-09-08 01:21:03 +0700

    feat: add cache server call

--- src/trpc/server.ts
+++ src/trpc/server.ts
@@ -7,6 +7,7 @@ import { cache } from "react";
 import { createCaller, type AppRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 import { createQueryClient } from "./query-client";
+import { DAYS } from "~/lib/constant";
 
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
@@ -15,6 +16,8 @@ import { createQueryClient } from "./query-client";
 const createContext = cache(() => {
   const heads = new Headers(headers());
   heads.set("x-trpc-source", "rsc");
+  const ONE_DAY = 1 * DAYS;
+  heads.set("cache-control", `s-maxage=1, stale-while-revalidate=${ONE_DAY}`);
 
   return createTRPCContext({
     headers: heads,
