47059daf87737dd355507933617d6bcf9daa765d
dotslashf <38921923+dotslashf@users.noreply.github.com>
2024-09-10 00:38:22 +0700

    feat: open statistic for public

--- src/app/statistics/page.tsx
+++ src/app/statistics/page.tsx
@@ -2,7 +2,6 @@ import { type Metadata } from "next";
 import Layout from "~/components/Layout";
 import { HydrateClient } from "~/trpc/server";
 import { StatisticsPage } from "../_components/StatisticsPage";
-import HOCAuth from "~/components/HOCAuth";
 import { Suspense } from "react";
 import { Skeleton } from "~/components/ui/skeleton";
 
@@ -13,20 +12,18 @@ export const metadata: Metadata = {
 export default function Statistics() {
   return (
     <HydrateClient>
-      <HOCAuth>
-        <Layout>
-          <Suspense
-            fallback={
-              <div className="grid w-full grid-cols-1 gap-4 lg:grid-cols-2">
-                <Skeleton className="h-80 w-full" />
-                <Skeleton className="h-80 w-full" />
-              </div>
-            }
-          >
-            <StatisticsPage />
-          </Suspense>
-        </Layout>
-      </HOCAuth>
+      <Layout>
+        <Suspense
+          fallback={
+            <div className="grid w-full grid-cols-1 gap-4 lg:grid-cols-2">
+              <Skeleton className="h-80 w-full" />
+              <Skeleton className="h-80 w-full" />
+            </div>
+          }
+        >
+          <StatisticsPage />
+        </Suspense>
+      </Layout>
     </HydrateClient>
   );
 }
