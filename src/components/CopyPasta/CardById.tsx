cc00d7dfbb1deee4edd66c25d216f924643e4cf9
dotslashf <38921923+dotslashf@users.noreply.github.com>
2024-09-13 00:29:00 +0700

    fix: missing profile url

--- src/components/CopyPasta/CardById.tsx
+++ src/components/CopyPasta/CardById.tsx
@@ -149,7 +149,7 @@ export default function CardById({ copyPasta }: CardProps) {
             <div className="flex w-full flex-col justify-evenly">
               <span className="text-sm font-normal">Diarsipkan oleh:</span>
               <Link
-                href={`/user/${copyPasta.createdBy?.username}`}
+                href={`/user/${copyPasta.createdBy?.username ?? copyPasta.createdBy?.id}`}
                 className={cn(
                   badgeVariants({
                     variant: "ghost",
